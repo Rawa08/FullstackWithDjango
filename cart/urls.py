e1dd413591f37bc9c5a1e9c082184b08a98f8802
Rawa08 <rawa_aref@hotmail.com>
2020-03-03 22:35:52 +0000

    Rendering cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from .views import my_cart, add_product, adjust
 
 
 urlpatterns = [
-     url(r'^$', my_cart, name='my_cart'),
+    url(r'^$', my_cart, name='my_cart'),
     url(r'^add/(?P<id>\d+)', add_product, name='add_product'),
     url(r'^adjust/(?P<id>\d+)', adjust, name='adjust'),
 ]
