fe516af964a0fbe2d9820146465df0b8b1e4a70e
Rawa08 <rawa_aref@hotmail.com>
2020-03-05 15:35:40 +0000

    bug fix

--- myfragrance/settings.py
+++ myfragrance/settings.py
@@ -88,15 +88,17 @@ WSGI_APPLICATION = 'myfragrance.wsgi.application'
 
 
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 else:
-    print("Database URL not found")
+    print('Database URL not found. Using SQLite instead')
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}    
-
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
