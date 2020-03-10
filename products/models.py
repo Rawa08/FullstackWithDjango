9127413b17f02e33c29c8549a4ec99a51c42dfb9
Rawa08 <rawa_aref@hotmail.com>
2020-03-10 13:53:13 +0000

    changing choicevalue for gender in model

--- products/models.py
+++ products/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Perfume(models.Model):
     genderChoice = (
-        ('U','Unisex'), ('M', 'Male'), ('F', 'Female'))
+        ('unisex','unisex'), ('male', 'male'), ('female', 'female'))
     volumeChoice = (('25',25), ('30',30),('40', 40),('50', 50), ('65', 65),('70', 70), ('90',90), 
     ('100',100), ('125',125), ('130',130), ('150',150), ('175',175), ('200',200), ('300',300))
-    name = models.CharField(max_length=30, default='')
-    brand = models.CharField(max_length=30, default='')
-    gender = models.CharField(max_length=5, choices=genderChoice, default='U')
+    name = models.CharField(max_length=50, default='')
+    brand = models.CharField(max_length=40, default='')
+    gender = models.CharField(max_length=7, choices=genderChoice, default='unisex')
     volume = models.CharField(choices=volumeChoice, max_length=5)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
