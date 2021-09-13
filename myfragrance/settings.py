2c1a8485fdf9b1c3b3d27eb28ef1f521e1ded165
Rawa Aref <48259778+Rawa08@users.noreply.github.com>
2021-09-13 12:26:18 +0200

    Update Allowed hosts

--- myfragrance/settings.py
+++ myfragrance/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['myfragrance.herokuapp.com']
+ALLOWED_HOSTS =  os.getenv('ALLOWED_HOSTS')
 
 
 # Application definition
